d02299c785d8690a799d44e23742ef4e74514076
CoolPlayLin <CoolPlayLin@qq.com>
2024-02-02 13:02:33 +0800

    feat: rewrite App with FC :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from "react";
 import SelectMusic from "./components/selectMusic";
 import CurrentManifests from "./components/currentMusic";
 import "./App.css";
@@ -11,36 +10,34 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const { Header, Content, Footer } = Layout;
 
-class App extends React.Component {
-  render(): ReactNode {
-    return (
-      <Layout>
-        <Header className="bg-white" style={{ padding: 0, paddingLeft: 30 }}>
-          <Space className="text-center">
-            <Link to="/">主页</Link>
-            <Link to="/current">当前歌单</Link>
-            <Link to="/select">所有方案</Link>
-            <Link to="/music">已过审歌曲</Link>
-          </Space>
-        </Header>
-        <Content>
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/current" element={<CurrentManifests />} />
-            <Route path="/select" element={<SelectMusic />} />
-            <Route path="/music" element={<Music />} />
-          </Routes>
-        </Content>
-        <Footer className="text-center">
-          This website © 2024 created by{" "}
-          <a target="_blank" href="https://github.com/CoolPlayLin">
-            CoolPlayLin
-          </a>
-        </Footer>
-        <SpeedInsights />
-      </Layout>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Header className="bg-white" style={{ padding: 0, paddingLeft: 30 }}>
+        <Space className="text-center">
+          <Link to="/">主页</Link>
+          <Link to="/current">当前歌单</Link>
+          <Link to="/select">所有方案</Link>
+          <Link to="/music">已过审歌曲</Link>
+        </Space>
+      </Header>
+      <Content>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/current" element={<CurrentManifests />} />
+          <Route path="/select" element={<SelectMusic />} />
+          <Route path="/music" element={<Music />} />
+        </Routes>
+      </Content>
+      <Footer className="text-center">
+        This website © 2024 created by{" "}
+        <a target="_blank" href="https://github.com/CoolPlayLin">
+          CoolPlayLin
+        </a>
+      </Footer>
+      <SpeedInsights />
+    </Layout>
+  );
+};
 
 export default App;
